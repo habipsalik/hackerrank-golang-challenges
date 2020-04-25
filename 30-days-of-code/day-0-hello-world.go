747c98473aa02867bd93a3e575326b987b08e743
habipsalik <hhabipsalik@gmail.com>
2020-04-26 01:14:35 +0300

    create solution for day-0

---
+++ 30-days-of-code/day-0-hello-world.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	//Enter your code here. Read input from STDIN. Print output to STDOUT
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	input := scanner.Text()
+	err := scanner.Err()
+	checkError(err)
+
+	fmt.Println("Hello, World.")
+	fmt.Println(input)
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
