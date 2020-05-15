8afda93a8fc78dc8744158b095f1e53791306527
habipsalik <hhabipsalik@gmail.com>
2020-05-15 19:17:12 +0300

    create solution for day-29

---
+++ 30-days-of-code/Day-29-bitwise-and.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	tTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	t := int32(tTemp)
+
+	for tItr := 0; tItr < int(t); tItr++ {
+		nk := strings.Split(readLine(reader), " ")
+
+		nTemp, err := strconv.ParseInt(nk[0], 10, 64)
+		checkError(err)
+		n := int32(nTemp)
+
+		kTemp, err := strconv.ParseInt(nk[1], 10, 64)
+		checkError(err)
+		k := int32(kTemp)
+
+		var max int32
+
+		for a := int32(1); a <= n; a++ {
+			for b := int32(1); b < a; b++ {
+
+				result := a & b
+				if result < k && result > max {
+					max = result
+				}
+			}
+		}
+
+		fmt.Println(max)
+	}
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
